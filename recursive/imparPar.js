e80ebe3fe78690157aa5b8d1aa626eab85417f84
Ga5000 <gbr.lisboa@gmail.com>
2024-02-28 16:13:12 -0300

    organized files

--- recursive/imparPar.js
+++ recursive/imparPar.js
@@ -2,5 +2,5 @@ function imparPar(n, number) {
     return (number <= n) ? (number === 0) ? "par" : imparPar(n, (number < 0) ? -number - 1 : number - 1) : "negativo";
 }
 
-let n = -10, number = 0;
+let n = 10, number = 0;
 console.log(imparPar(n, number));
